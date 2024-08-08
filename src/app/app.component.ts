8a8a3c8ce3b7fb76939c77979e30dbdcf9de2cc7
Henrique-Moura <henrique_hgm1996@hotmail.com>
2024-08-08 09:17:01 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: 'app-root', /* Esse selector quer é utlizado ontem o template será injetado, nesse caso, está sendo aplicado no index.html */
+  templateUrl: './app.component.html', /* Template HTML que será rederizado pelo AppComponent */
+  styleUrls: ['./app.component.less'] /* Estilo Utilizado */
 })
 export class AppComponent {
-  title = 'studyAngular';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; /* Prepara a execução pra ser rodada em um Browser */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
