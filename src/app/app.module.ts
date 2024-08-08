9597b7b01198e5ba40b8961552403fd84a9fd9cb
henriquemoura <henrique.moura@bnpsolucoes.com.br>
2024-08-08 11:45:20 -0300

    Introducao - Modulos (ngModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; /* Prepara a execução pra ser rodada em um Browser */
+import { BrowserModule } from '@angular/platform-browser'; /* Prepara a execução para ser rodada em um navegador */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    XyzComponent
+    XyzComponent /* Primeira maneira de renderizar um componente no app: adicionando-o às declarações. Isso é recomendado apenas para componentes, diretivas e pipes que pertencem ao módulo específico. No caos, esse component não deveria estar declarado aqui, mas apenas o AppComponent */
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HelloWorldModule, /* Segunda maneira de utilizar um componente no app: importando o módulo que o exporta. O módulo deve exportar o componente para que ele possa ser usado aqui. */
   ],
   providers: [],
   bootstrap: [AppComponent]
